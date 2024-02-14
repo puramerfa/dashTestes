cb1d0e780ce2b4b1dbfe03067967acd06eb8e8e9
Guilherme Mello <guilhermetts0@gmail.com>
2024-02-14 04:01:15 -0300

    config server

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', (req, res) => {
 
 
 app.get('/executar-comando', (req, res) => {
-    const caminhoDaPasta = 'C:/Users/Guilherme Mello/Documents/projeto-qa-guilherme-mello/Cypress'; 
-    const comando = 'npx cypress open'; 
+    const caminhoDaPasta = './'; 
+    const comando = 'npm run rodarTestes'; 
     // const comando = 'npx cypress run --record --key c1d53d8d-c5d4-441d-9685-099a1921b515'; 
 
    
@@ -23,6 +23,6 @@ app.get('/executar-comando', (req, res) => {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, '192.168.0.102', () => {
+app.listen(port, 'localhost', () => {
     console.log(`Servidor iniciado  localhost:${port}`);
 });
